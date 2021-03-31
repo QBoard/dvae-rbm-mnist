a89c746e1672e7a9a159656242e14dee68bad189
Konstantin Avchaciov <k.avchachov@gmail.com>
2021-03-31 19:22:32 +0300

    Gibbs sampler acceleration

--- evalm.py
+++ evalm.py
@@ -8,13 +8,13 @@ from hypers import *
 # Estimation of logZ, Z - partition function
 # Implements Annealed Importance Sampling (AIS)
 def calc_logZ(model):
-    s = Sampler(model.z_dim)
+    s = model.sampler   # Sampler(model.z_dim)
     c = [x/ais_num_dists for x in range(0,ais_num_dists+1)]
     logZ = 0
-   
+
     if s.name == 'gibbs':
         logs_w = []
-    
+
         for j in range(ais_num_samples):
             logw = 0
             for i in range(ais_num_dists):
@@ -22,22 +22,22 @@ def calc_logZ(model):
                 tmp = torch.einsum('bi,ij,bj->b',z[0,:model.z_dim].view(1,model.z_dim),
                                     model.W, z[0,model.z_dim:].view(1,model.z_dim)).item()
                 tmp += torch.mm(z[0].view(1,2*model.z_dim), model.b.view(2*model.z_dim,1)).item()
-     
+
                 tmp = tmp * (c[i] - c[i+1])
                 logw += tmp
             logs_w.append(logw)
-    
+
         logs_w = [math.exp(lw) for lw in logs_w]
         logw = math.log(sum(logs_w)/ais_num_samples)
-    
+
         logZ = logw + math.log(2.0) * model.z_dim * 2
-    
+
 #    print("log Z =", logZ)
-    
+
     return logZ
 
 # Model evaluation
-def evaluate_model(model, data_loader):
+def evaluate_model(model, data_loader, add_logZ=True):
     model.eval()
     total_size = 0
     total_loss = 0
@@ -46,28 +46,31 @@ def evaluate_model(model, data_loader):
             x_size = x.size(0)
             total_size += x_size
             x = x.to(device).view(-1, image_size)
-            
+
             x_reconst, ent, p_ph = model(x)
-            
+
             # Reconstruction loss
-            reconst_loss = F.binary_cross_entropy(x_reconst, x, reduction='sum')        
-                
+            reconst_loss = F.binary_cross_entropy(x_reconst, x, reduction='sum')
+
             # Calculation of logZ
-            logZ = calc_logZ(model)
+            if add_logZ:
+                logZ = calc_logZ(model)
+            else:
+                logZ = 0.
             #print("log Z =", logZ)
-        
+
             # KL divergence
             kl_div = -ent + p_ph + logZ * x_size
             #print("entropy =", ent.item()/x_size)
             #print("pos term =", p_ph.item()/x_size)
             #print("kl div = ", kl_div.item()/x_size)
-        
+
             # Total loss
             loss = reconst_loss + kl_div
-         
+
             total_loss += loss.item()
-            
+
     # Average loss over dataset
     av_loss = total_loss / total_size
-        
+
     return av_loss
--- sampler.py
+++ sampler.py
@@ -1,33 +1,55 @@
 import torch
-import numpy as np
-import math
-import time
 
 # Hyperparameters
-from hypers import *
+from hypers import device
+
 
 # Gibbs Sampler class
 class GSampler:
     def __init__(self, z_dim, gc_num=30):
         self.name = 'gibbs'
-        self.z_dim = z_dim        
+        self.z_dim = z_dim
         self.gc_num = gc_num
         self.z = torch.ones(2 * z_dim, device=device) * 0.5
-        self.z = torch.bernoulli(self.z) 
-    
+        self.z = torch.bernoulli(self.z)
+
     def sample(self, W, b, num_samples):
         # Implements Gibbs sampler
         z_samples = torch.zeros(num_samples, 2 * self.z_dim).to(device)
-        
+
         for j in range(num_samples):
             for i in range(self.gc_num):
-                pv = torch.sigmoid(torch.mm(self.z[self.z_dim:].view(1,self.z_dim), -W.t()) - b[:self.z_dim])
+                pv = torch.sigmoid(torch.mm(self.z[self.z_dim:].view(1, self.z_dim),
+                                            -W.t()) - b[:self.z_dim])
                 self.z[:self.z_dim] = torch.bernoulli(pv)
-                ph = torch.sigmoid(torch.mm(self.z[:self.z_dim].view(1,self.z_dim), -W) - b[self.z_dim:])
+                ph = torch.sigmoid(torch.mm(self.z[:self.z_dim].view(1, self.z_dim), -W)
+                                   - b[self.z_dim:])
                 self.z[self.z_dim:] = torch.bernoulli(ph)
             z_samples[j] = self.z
-            
-        #print("z =", self.z)
-            
+
+        # print("z =", self.z)
+
         return z_samples
 
+
+class GSampler_fast:
+    def __init__(self, z_dim, gc_num=30):
+        self.name = 'gibbs'
+        self.z_dim = z_dim
+        self.gc_num = gc_num
+
+    def sample(self, W, b, num_samples):
+        # Implements Gibbs sampler
+        z_h = torch.ones(num_samples, self.z_dim, device=device) * 0.5
+        z_h = torch.bernoulli(z_h)
+
+        z_v = torch.ones(num_samples, self.z_dim, device=device) * 0.5
+        z_v = torch.bernoulli(z_v)
+
+        for i in range(self.gc_num):
+            pv = torch.sigmoid(torch.mm(z_h, -W.t()) - b[:self.z_dim])
+            z_v = torch.bernoulli(pv)
+            ph = torch.sigmoid(torch.mm(z_v, -W) - b[self.z_dim:])
+            z_h = torch.bernoulli(ph)
+
+        return torch.cat((z_v, z_h), 1)
